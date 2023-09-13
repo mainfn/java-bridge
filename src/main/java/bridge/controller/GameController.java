69323a97c0f55f123361b3e3f1e7564fb4da2b2e
mainfn <_@mainfn.dev>
2023-09-13 14:22:15 +0900

    refactor: GameController 로직 분리

--- src/main/java/bridge/controller/GameController.java
+++ src/main/java/bridge/controller/GameController.java
@@ -1,7 +1,6 @@
 package bridge.controller;
 
 import bridge.domain.BridgeGame;
-import bridge.domain.BridgeGameResult;
 import bridge.domain.BridgeMaker;
 import bridge.domain.BridgeMap;
 import bridge.domain.MovePosition;
@@ -31,13 +30,13 @@ public final class GameController {
     final BridgeGame bridgeGame = makeBridgeGame();
 
     // 2. 게임 실행(입력을 받은 위치로 이동, 다리 끝까지 or 이동이 실패할 때까지)
-    final BridgeGameResult bridgeGameResult = play(bridgeGame);
+    play(bridgeGame);
 
     // 3. 결과 출력
-    outputView.printResult(bridgeGameResult);
+    outputView.printResult(bridgeGame.getGameResult());
   }
 
-  private BridgeGameResult play(final BridgeGame bridgeGame) {
+  private void play(final BridgeGame bridgeGame) {
 
     while (bridgeGame.isPlaying()) {
       final String movePositionString = inputView.readMoving();
@@ -47,6 +46,11 @@ public final class GameController {
       final MoveRecords moveRecords = bridgeGame.getMoveRecords();
       outputView.printMap(moveRecords);
     }
+
+    retryIfFailed(bridgeGame);
+  }
+
+  private void retryIfFailed(final BridgeGame bridgeGame) {
     if (bridgeGame.isFailed()) {
       final String command = inputView.readGameCommand();
       if ("R".equals(command)) {
@@ -54,8 +58,6 @@ public final class GameController {
         play(bridgeGame);
       }
     }
-
-    return bridgeGame.getGameResult();
   }
 
   private BridgeGame makeBridgeGame() {
